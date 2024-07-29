e57938b0e3d41c29bdec178ee43708653bdaf158
j25ng <jsan2861@gmail.com>
2024-07-29 15:19:41 +0900

    add

--- src/movie/api/call.py
+++ src/movie/api/call.py
@@ -2,13 +2,13 @@ import requests
 import os
 import pandas as pd
 
-def gen_url(dt="20240101"):
+def gen_url(dt):
     base_url = "http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json"
     key = get_key()
     url = f"{base_url}?key={key}&targetDt={dt}"
     return url
 
-def req(dt="20120101"):
+def req(dt):
     url = gen_url(dt)
     r = requests.get(url)
     code = r.status_code
@@ -20,20 +20,20 @@ def get_key():
     key = os.getenv("MOVIE_API_KEY")
     return key
 
-def req2list(loadDt='20120101'):
-    _, data = req()
+def req2list(dt):
+    _, data = req(dt)
     # data.get('boxOfficeResult').get('dailyBoxOfficeList')
     l = data['boxOfficeResult']['dailyBoxOfficeList']
     return l
 
-def list2df(loadDt='20120101'):
-    l = req2list()
+def list2df(dt):
+    l = req2list(dt)
     df = pd.DataFrame(l)
     return df
 
-def save2df(loadDt='20120101'):
-    df = list2df(loadDt)
-    df['loadDt'] = "20120101"
+def save2df(dt='20210101'):
+    df = list2df(dt)
+    df['loadDt'] = dt 
     df.to_parquet('~/tmp/test_parquet/', partition_cols=['loadDt'])
     return df
 
