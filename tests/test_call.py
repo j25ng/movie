51b0f2c33a5b788afd7a5d6a295a28ebaf40b05c
j25ng <jsan2861@gmail.com>
2024-07-29 15:20:10 +0900

    add

--- tests/test_call.py
+++ tests/test_call.py
@@ -1,27 +1,29 @@
 from movie.api.call import gen_url, req, get_key, req2list, list2df, save2df
 import pandas as pd
 
+dt = '20120101'
+
 def test_hide_key():
     key = get_key()
     assert key
 
 def test_gen_url():
-    url = gen_url()
+    url = gen_url(dt)
     assert "http" in url
     assert "kobis" in url
 
 def test_req():
-    code, data = req()
+    code, data = req(dt)
     assert code == 200
 
 def test_req2list():
-    l = req2list()
+    l = req2list(dt)
     assert len(l) > 0
     v = l[0]
     assert 'rnum' in v.keys()
 
 def test_list2df():
-    df = list2df()
+    df = list2df(dt)
     assert isinstance(df, pd.DataFrame)
     assert 'rnum' in df.columns
     assert 'openDt' in df.columns
@@ -29,6 +31,6 @@ def test_list2df():
     assert 'audiAcc' in df.columns
 
 def test_save2df():
-    df = save2df()
+    df = save2df(dt)
     assert isinstance(df, pd.DataFrame)
     assert 'loadDt' in df.columns
