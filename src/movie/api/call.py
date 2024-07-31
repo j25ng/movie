7702b52bbbe99fa2ca54f713dd62a355bad86d80
j25ng <jsan2861@gmail.com>
2024-07-31 09:23:16 +0900

    commit

--- src/movie/api/call.py
+++ src/movie/api/call.py
@@ -45,7 +45,9 @@ def apply_type2df(load_dt='20120101', path="~/tmp/test_parquet"):
 
     num_cols = ['rnum', 'rank', 'rankInten', 'salesAmt', 'audiCnt', 'audiAcc', 'scrnCnt', 'showCnt', 'salesShare', 'salesInten', 'salesChange', 'audiInten', 'audiChange']
 
-    for c in num_cols:
-        df[c] = pd.to_numeric(df[c])
+    #for c in num_cols:
+    #    df[c] = pd.to_numeric(df[c])
 
-    return df, num_cols
+    df[num_cols] = df[num_cols].apply(pd.to_numeric)
+
+    return df
