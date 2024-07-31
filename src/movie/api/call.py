a514bc91f01aa5c30f2de7993ee65339976275ab
j25ng <jsan2861@gmail.com>
2024-07-31 16:52:16 +0900

    push

--- src/movie/api/call.py
+++ src/movie/api/call.py
@@ -2,14 +2,18 @@ import requests
 import os
 import pandas as pd
 
-def gen_url(dt='20120101'):
+def gen_url(dt='20120101', url_param = {"multiMovieYn": "N"}):
     base_url = "http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json"
     key = get_key()
     url = f"{base_url}?key={key}&targetDt={dt}"
+
+    for k, v in url_param.items():
+        url = url + f"&{k}={v}"
+
     return url
 
-def req(dt='20120101'):
-    url = gen_url(dt)
+def req(dt='20120101', url_param={}):
+    url = gen_url(dt, url_param)
     r = requests.get(url)
     code = r.status_code
     data = r.json()
@@ -20,19 +24,19 @@ def get_key():
     key = os.getenv("MOVIE_API_KEY")
     return key
 
-def req2list(dt='20120101'):
-    _, data = req(dt)
+def req2list(dt='20120101', url_param={}):
+    _, data = req(dt, url_param)
     # data.get('boxOfficeResult').get('dailyBoxOfficeList')
     l = data['boxOfficeResult']['dailyBoxOfficeList']
     return l
 
-def list2df(dt='20120101'):
-    l = req2list(dt)
+def list2df(dt='20120101', url_param={}):
+    l = req2list(dt, url_param)
     df = pd.DataFrame(l)
     return df
 
-def save2df(dt='20120101'):
-    df = list2df(dt)
+def save2df(dt='20120101', url_param={}):
+    df = list2df(dt, url_param)
     df['loadDt'] = dt 
     df.to_parquet('~/tmp/test_parquet/', partition_cols=['loadDt'])
     return df
